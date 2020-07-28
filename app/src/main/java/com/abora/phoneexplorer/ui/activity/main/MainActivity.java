ceb82ffd3ee52261521fa7a0656d2ffe3aa9774f
Abora <https://github.com/abora97>
2020-07-28 22:54:38 +0200

    add MainViewModel

--- app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainActivity.java
@@ -1,16 +1,53 @@
 package com.abora.phoneexplorer.ui.activity.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.abora.phoneexplorer.R;
+import com.abora.phoneexplorer.model.PhoneResponse;
+import com.abora.phoneexplorer.network.APIInterface;
+import com.abora.phoneexplorer.network.ApiClient;
+import com.abora.phoneexplorer.util.Constants;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        init();
+
     }
+
+
+
+    private void init() {
+
+        mainViewModel.getPhones();
+
+        mainViewModel.phoneResponseMutableLiveData.observe(this, new Observer<List<PhoneResponse>>() {
+            @Override
+            public void onChanged(List<PhoneResponse> phoneResponses) {
+                Toast.makeText(MainActivity.this, ""+ phoneResponses.get(0).getDeviceName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 }
---
+++ app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainViewModel.java
@@ -0,0 +1,38 @@
+package com.abora.phoneexplorer.ui.activity.main;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.abora.phoneexplorer.model.PhoneResponse;
+import com.abora.phoneexplorer.network.APIInterface;
+import com.abora.phoneexplorer.network.ApiClient;
+import com.abora.phoneexplorer.util.Constants;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainViewModel extends ViewModel {
+
+    MutableLiveData<List<PhoneResponse>> phoneResponseMutableLiveData = new MutableLiveData<>();
+    private static final String TAG = "MainViewModel";
+
+     void getPhones() {
+        final APIInterface apiService = ApiClient.getClient().create(APIInterface.class);
+         Call<List<PhoneResponse>> call = apiService.getPhone(Constants.KEY, Constants.SAMSUNG);
+         call.enqueue(new Callback<List<PhoneResponse>>() {
+            @Override
+            public void onResponse(Call<List<PhoneResponse>> call, Response<List<PhoneResponse>> response) {
+                phoneResponseMutableLiveData.setValue(response.body());
+            }
+            @Override
+            public void onFailure(Call<List<PhoneResponse>> call, Throwable t) {
+                Log.d(TAG, t.toString());
+            }
+        });
+    }
+}
