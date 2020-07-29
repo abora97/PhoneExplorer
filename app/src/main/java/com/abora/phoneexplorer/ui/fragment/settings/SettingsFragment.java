6b8f6aacf82fcd0dd9ec44a31a1a76c1cca6c7e1
Abora <https://github.com/abora97>
2020-07-29 20:16:47 +0200

    finish SettingsFragment

--- app/src/main/java/com/abora/phoneexplorer/ui/fragment/settings/SettingsFragment.java
+++ app/src/main/java/com/abora/phoneexplorer/ui/fragment/settings/SettingsFragment.java
@@ -1,66 +1,62 @@
 package com.abora.phoneexplorer.ui.fragment.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.abora.phoneexplorer.R;
+import com.abora.phoneexplorer.util.Constants;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SettingsFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
+    @BindView(R.id.edMaxNum)
+    EditText edMaxNum;
+    @BindView(R.id.btnSave)
+    Button btnSave;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    SharedPreferences.Editor editor;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
+        btnSave.setOnClickListener(this);
+        SharedPreferences sharedpreferences = getContext().getSharedPreferences(Constants.MyPREFERENCES, Context.MODE_PRIVATE);
+        editor = sharedpreferences.edit();
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int n = 10;
+        String s = edMaxNum.getText().toString();
+        if (s.equals(null) || s.equals("")) {
+            Toast.makeText(getContext(), "please Enter Number", Toast.LENGTH_SHORT).show();
+        } else {
+            n = Integer.parseInt(s);
+            if (n < 1 || n > 100) {
+                Toast.makeText(getContext(), "Enter number between 1 and 100", Toast.LENGTH_SHORT).show();
+                edMaxNum.setText("");
+            }else {
+                Toast.makeText(getContext(), "Saving", Toast.LENGTH_SHORT).show();
+                editor.putInt(Constants.LIMIT, n);
+                editor.commit();
+            }
+        }
+
     }
 }
