c031d7144f416071da44f08ca7f2b5d88070cee4
Abora <https://github.com/abora97>
2020-07-29 02:38:09 +0200

    add bottom bar

--- app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.abora.phoneexplorer.ui.activity.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,6 +8,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.abora.phoneexplorer.R;
@@ -14,6 +16,7 @@ import com.abora.phoneexplorer.model.PhoneResponse;
 import com.abora.phoneexplorer.network.APIInterface;
 import com.abora.phoneexplorer.network.ApiClient;
 import com.abora.phoneexplorer.util.Constants;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 
@@ -21,8 +24,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private BottomNavigationView navView;
     MainViewModel mainViewModel;
 
     @Override
@@ -30,11 +34,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
         init();
 
     }
-
-
 
     private void init() {
 
@@ -47,6 +51,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.navHome:
+
+                Toast.makeText(MainActivity.this, "name home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navSearch:
+
+                Toast.makeText(MainActivity.this, "navSearch", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navFavorite:
+
+                Toast.makeText(MainActivity.this, "navFavorite", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 
 
