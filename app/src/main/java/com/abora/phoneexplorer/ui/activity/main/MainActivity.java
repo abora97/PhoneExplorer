078ad8d720d2894926e5a05a48503b4b083ee660
Abora <https://github.com/abora97>
2020-07-29 02:56:46 +0200

    add tabs

--- app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/abora/phoneexplorer/ui/activity/main/MainActivity.java
@@ -2,6 +2,9 @@ package com.abora.phoneexplorer.ui.activity.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
@@ -15,6 +18,9 @@ import com.abora.phoneexplorer.R;
 import com.abora.phoneexplorer.model.PhoneResponse;
 import com.abora.phoneexplorer.network.APIInterface;
 import com.abora.phoneexplorer.network.ApiClient;
+import com.abora.phoneexplorer.ui.fragment.compare.CompareFragment;
+import com.abora.phoneexplorer.ui.fragment.explore.ExploreFragment;
+import com.abora.phoneexplorer.ui.fragment.settings.SettingsFragment;
 import com.abora.phoneexplorer.util.Constants;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -41,13 +47,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
     private void init() {
-
+        openFragment(new ExploreFragment(), "home");
         mainViewModel.getPhones();
 
         mainViewModel.phoneResponseMutableLiveData.observe(this, new Observer<List<PhoneResponse>>() {
             @Override
             public void onChanged(List<PhoneResponse> phoneResponses) {
-                Toast.makeText(MainActivity.this, ""+ phoneResponses.get(0).getDeviceName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "" + phoneResponses.get(0).getDeviceName(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -59,20 +65,27 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         switch (item.getItemId()) {
             case R.id.navHome:
-
-                Toast.makeText(MainActivity.this, "name home", Toast.LENGTH_SHORT).show();
+                openFragment(new ExploreFragment(), "ExploreFragment");
                 break;
-            case R.id.navSearch:
-
-                Toast.makeText(MainActivity.this, "navSearch", Toast.LENGTH_SHORT).show();
+            case R.id.navCompare:
+                openFragment(new CompareFragment(), "CompareFragment");
                 break;
-            case R.id.navFavorite:
-
-                Toast.makeText(MainActivity.this, "navFavorite", Toast.LENGTH_SHORT).show();
+            case R.id.navSettings:
+                openFragment(new SettingsFragment(), "SettingsFragment");
                 break;
         }
         return true;
     }
 
+    private void openFragment(Fragment fragment, String tag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentByTag(tag) == null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            ft.replace(R.id.nav_host_fragment, fragment, tag).commit();
+        }
+        List<Fragment> fragments = fragmentManager.getFragments();
+        Log.e("Fragments Quantity", String.valueOf(fragments.size()));
+        fragmentManager.beginTransaction().show(fragment).commit();
+    }
 
 }
---
+++ app/src/main/java/com/abora/phoneexplorer/ui/fragment/explore/ExploreFragment.java
@@ -0,0 +1,23 @@
+package com.abora.phoneexplorer.ui.fragment.explore;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abora.phoneexplorer.R;
+
+public class ExploreFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+
+
+        return view;
+    }
+}
